341beb5d3966ee3b8e5453ccb791ef75e0ab57e3
hachadorian <hachadorian98@gmail.com>
2022-06-23 14:20:17 -0400

    correct upgrade to react 18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const container = document.getElementById("app");
-const root = createRoot(container);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
